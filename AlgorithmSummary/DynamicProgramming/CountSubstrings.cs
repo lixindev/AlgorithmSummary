efb0b7a0f2c9592aca83a75361e4bf080a53b4f2
lixindev <15910203729@163.com>
2023-03-27 15:18:42 +0800

    字符串动态规划

---
+++ AlgorithmSummary/DynamicProgramming/CountSubstrings.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AlgorithmSummary
+{
+    /// <summary>
+    /// 给你两个字符串 s 和 t ，请你找出 s 中的非空子串的数目，这些子串满足替换 一个不同字符 以后，
+    /// 是 t 串的子串。换言之，请你找到 s 和 t 串中 恰好 只有一个字符不同的子字符串对的数目。
+    /// 比方说， "computer" and "computation" 只有一个字符不同： 'e'/'a' ，所以这一对子字符串会给答案加 1 。
+    /// </summary>
+    public class CountSubstrings
+    {
+        /// <summary>
+        /// 题目要求求出字符串 s 与字符串 t 的连续子串中只差一个字符的子串对的数目，此时我们可以枚举 s 与 t 中不相等的字符对 (s[i],t[j])，
+        /// 并计算以 (s[i],t[j]) 构造的符合题意的子串数目即可。
+        /// 在实际计算时，设以字符 s[i] 与字符 t[j] 为终点且左侧连续相等的最大长度为 dpl[i][j]
+        /// 设以字符 s[i] 与字符 t[j] 为终点且右侧连续相等的最大长度为 dpr[i][j]
+        /// 以字符对 (s[i], t[j])构造子串，子串数目为 dpl[i][j] + 1 * dpr[i][j] + 1
+        /// 求dpl和dpr的思路可参考最长公共子序列（LongestCommonSubsequence）
+        /// s[i] = t[j]时，dpl[i][j] = dpl[i - 1][j - 1] + 1, dpr[i][j] = dpr[i + 1][j + 1] + 1
+        /// s[i] != t[j]时，dpl[i][j] = 0, dpr[i][j] = 0
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        public int Count_Substrings(string s, string t)
+        {
+            int m = s.Length, n = t.Length;
+            int[][] dpl = new int[m + 1][];
+            int[][] dpr = new int[m + 1][];
+            for (int i = 0; i <= m; i++)
+            {
+                dpl[i] = new int[n + 1];
+                dpr[i] = new int[n + 1];
+            }
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    dpl[i + 1][j + 1] = s[i] == t[j] ? (dpl[i][j] + 1) : 0;
+                }
+            }
+            for (int i = m - 1; i >= 0; i--)
+            {
+                for (int j = n - 1; j >= 0; j--)
+                {
+                    dpr[i][j] = s[i] == t[j] ? (dpr[i + 1][j + 1] + 1) : 0;
+                }
+            }
+            int ans = 0;
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (s[i] != t[j])
+                    {
+                        ans += (dpl[i][j] + 1) * (dpr[i + 1][j + 1] + 1);
+                    }
+                }
+            }
+            return ans;
+        }
+    }
+}
---
+++ AlgorithmSummary/DynamicProgramming/LongestCommonSubsequence.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AlgorithmSummary
+{
+    /// <summary>
+    /// 给定两个字符串 text1 和 text2，返回这两个字符串的最长 公共子序列 的长度。如果不存在 公共子序列 ，返回 0 。
+    /// 一个字符串的 子序列 是指这样一个新的字符串：它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。
+    /// </summary>
+    public class LongestCommonSubsequence
+    {
+        /// <summary>
+        /// 假设dp[i][j]表示text1[0 : i]和text2[0 : j]的最长公共子序列的长度
+        /// 初始状态：dp[0][j] = 0 且 dp[i][0] = 0
+        /// 当text1[i−1] = text2[j−1]时，dp[i][j] = dp[i - 1][j - 1] + 1;
+        /// 当text1[i−1] != text2[j−1]时，dp[i][j] = Max(dp[i][j - 1], dp[i - 1][j]);
+        /// </summary>
+        /// <param name="text1"></param>
+        /// <param name="text2"></param>
+        /// <returns></returns>
+        public int Longest_Common_Subsequence(string text1, string text2)
+        {
+            int m = text1.Length, n = text2.Length;
+            int[,] dp = new int[m + 1, n + 1];
+
+            for (int i = 1; i < m; i++)
+            {
+                for (int j = 1; j < n; j++)
+                {
+                    if (text1[i - 1] == text2[j - 1])
+                    {
+                        dp[i, j] = dp[i - 1, j - 1] + 1;
+                    }
+                    else
+                    {
+                        dp[i, j] = Math.Max(dp[i, j - 1], dp[i - 1, j]);
+                    }
+                }
+            }
+
+            return dp[m, n];
+        }
+    }
+}
